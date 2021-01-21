549e2ef4fa7e4220ed67abec386620a73691e958
thiago ribeiro <thiagogr71@gmail.com>
2021-01-21 16:47:33 -0300

    Setup users table

---
+++ db/migrations/20210121162921_create_users_table.js
@@ -0,0 +1,17 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('users', (table) => {
+    table.string('name');
+
+    table.string('email');
+
+    table.string('password');
+
+    table.integer('role');
+
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('users');
+};
---
+++ src/constants/user.js
@@ -0,0 +1,6 @@
+module.exports = {
+  id: 1,
+  name: 'thiago',
+  email: 'mail@mail.com',
+  password: 'coxinha123',
+};
